12027c484a23835b6d63f799724ed3edf23ade44
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-04-28 16:56:37 +0200

    creating file for policies

---
+++ td1/policies.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "qlearning.h"
+#include "functions.h"
+#include "mazeEnv.h"
+int policy_greedy(int state)
+{
+    return maxlist(qfunction[state], number_actions, true);
+}
+
+int policy_epsgreedy(int state, float epsi)
+{
+    int actio = 0;
+    float alea = ((float)rand()) / RAND_MAX;
+    if (alea < epsi)
+    {
+        actio = env_action_sample();
+    }
+    else
+    {
+        actio = maxlist(qfunction[state], number_actions, true);
+    }
+    return actio;
+}
