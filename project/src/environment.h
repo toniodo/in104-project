5ecd872e62d3ab6ea7f3c8fa87bf2961e1c8ae7e
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2022-05-22 16:37:15 +0200

    add linear reward & update matrix printing

--- project/src/environment.h
+++ project/src/environment.h
@@ -75,5 +75,5 @@ void reset_level();
 // make action to the environment
 envOutput make_action(action a);
 void gravity(int *new_row, int *new_col);
-
+double linear_reward(double min_reward, double max_reward, int N, int val);
 #endif
